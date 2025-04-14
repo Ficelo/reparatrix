bc9804ed629d4d39970af1cc8c0523bb94ad5ae7
syulcherr <156232164+syulcherr@users.noreply.github.com>
2025-04-14 17:11:12 +0200

    database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE dbreparatrix
+CREATE DATABASE dbreparatrix;
 
 CREATE TABLE User (
     id_user INT PRIMARY KEY AUTO_INCREMENT,
@@ -6,7 +6,7 @@ CREATE TABLE User (
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
     role VARCHAR(50) NOT NULL
-)
+);
 
 CREATE TABLE Prestataire (
     id_prestataire INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,7 +17,7 @@ CREATE TABLE Prestataire (
     user_id INT,
     siret VARCHAR(14),
     FOREIGN KEY (user_id) REFERENCES User(id_user)
-)
+);
 
 CREATE TABLE Client (
     id_client INT PRIMARY KEY AUTO_INCREMENT,
@@ -25,7 +25,7 @@ CREATE TABLE Client (
     localisation VARCHAR(100),
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES User(id_user)
-)
+);
 
 CREATE TABLE Service (
     id_service INT PRIMARY KEY AUTO_INCREMENT,
@@ -34,7 +34,7 @@ CREATE TABLE Service (
     prix DECIMAL(10,2),
     prestataire_id INT,
     FOREIGN KEY (prestataire_id) REFERENCES Prestataire(id_prestataire)
-)
+);
 
 CREATE TABLE Avis (
     id_avis INT PRIMARY KEY AUTO_INCREMENT,
@@ -44,7 +44,7 @@ CREATE TABLE Avis (
     commentaire TEXT,
     FOREIGN KEY (service_id) REFERENCES Service(id_service),
     FOREIGN KEY (user_id) REFERENCES User(id_user)
-)
+);
 
 CREATE TABLE Commande (
     id_commande INT PRIMARY KEY AUTO_INCREMENT,
@@ -55,7 +55,7 @@ CREATE TABLE Commande (
     date DATE,
     FOREIGN KEY (prestataire_id) REFERENCES Prestataire(id_prestataire),
     FOREIGN KEY (client_id) REFERENCES Client(id_client)
-)
+);
 
 CREATE TABLE Message (
     id_message INT PRIMARY KEY AUTO_INCREMENT,
@@ -65,4 +65,4 @@ CREATE TABLE Message (
     date DATETIME,
     FOREIGN KEY (expéditeur) REFERENCES User(id_user),
     FOREIGN KEY (destinataire) REFERENCES User(id_user)
-)
+);
