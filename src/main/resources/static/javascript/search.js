856702eec0352a67ad2de4de972dce1ab0de2593
Ficelo <teiva.trillard@gmail.com>
2025-04-28 17:52:37 +0200

    Recherche "avancée" de prestataires, back et front

--- src/main/resources/static/javascript/search.js
+++ src/main/resources/static/javascript/search.js
@@ -1,9 +1,11 @@
 function searchForProfession(inputId) {
 
     let input = document.getElementById(inputId);
+    let distanceMax = document.getElementById("distanceRange")
+    let noteMin = document.getElementById("noteRange")
     const resultats = document.getElementById("results")
 
-    fetch('/api/prestataires/profession/' + input.value)
+    fetch('/api/prestataires?profession=' + input.value + "&noteMin=" + noteMin.value + "&distanceMax=" + distanceMax.value)
         .then(reponse => reponse.json())
         .then(prestas => {
             const prestaList = document.createElement('ul');
@@ -17,5 +19,20 @@ function searchForProfession(inputId) {
                });
         })
         .catch( err => console.error('Error fetching prestataires:', err))
+
+}
+
+function updateSearchValueDisplay(displayId, rangeId, suffix) {
+
+
+
+    const display = document.getElementById(displayId);
+    console.log(display.innerText)
+    const range = document.getElementById(rangeId);
+
+    let innerText = display.innerText.split(":")
+    display.innerText = innerText[0] + " : " + range.value + suffix;
+
+    //console.log(range.value)
 
 }
