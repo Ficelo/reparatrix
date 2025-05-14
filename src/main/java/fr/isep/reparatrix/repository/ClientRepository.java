7e781908aadadbd836e70fb448c9857b6e034d60
Ficelo <teiva.trillard@gmail.com>
2025-05-14 10:49:55 +0200

    création de compte (sans style comme d'hab)

--- src/main/java/fr/isep/reparatrix/repository/ClientRepository.java
+++ src/main/java/fr/isep/reparatrix/repository/ClientRepository.java
@@ -3,5 +3,10 @@ package fr.isep.reparatrix.repository;
 import fr.isep.reparatrix.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> getClientByUserId(Long userId);
+
 }
--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -4,10 +4,11 @@ async function login() {
     const userReponse = getUserByEmail(email);
 
     // ça vérifie pas grand chose pour l'instant c'est juste pour tester
-    userReponse.then((user) => {
+    userReponse.then(async (user) => {
         console.log(user);
         localStorage.setItem("user", JSON.stringify(user));
         console.log("user in localstorage", JSON.parse(localStorage.getItem("user")));
+        await getClientByUserId(user.id);
         window.location.href = "/";
     })
 
@@ -17,6 +18,21 @@ async function getUserByEmail(email) {
     const response = await fetch('/api/users/email/' + email);
     if (!response.ok) throw new Error("Http error" + response.status);
     return await response.json();
+}
+
+async function getPrestataireByUserId(userId) {
+    // Todo : implémenter
+}
+
+async function getClientByUserId(userId) {
+    const response = await fetch("/api/clients/userId/" + userId);
+    if (!response.ok) throw new Error("Http error " + response.status);
+    const client = await response.json();
+    console.log(client);
+
+    client.forEach(cli => {
+        localStorage.setItem("client", JSON.stringify(cli));
+    });
 }
 
 // Vérifier que on est connecté
